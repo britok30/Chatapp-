837f418413e379941f7f9c8911570e31731d15d6
Kelvin Brito <britok30@gmail.com>
2020-08-27 01:20:59 -0400

    styling main page

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
-    return <div>Join</div>;
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    return (
+        <div className="join-hero">
+            <div className="joinOuterContainer">
+                <div className="joinInnerContainer">
+                    <h1 className="heading">Welcome to ChatApp</h1>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="joinInput"
+                            type="text"
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="joinInput"
+                            type="text"
+                            value={room}
+                            onChange={(e) => {
+                                setRoom(e.target.value);
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Join;
