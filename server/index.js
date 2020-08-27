5490ac4dd9ccc025dbc58219ec0507f87c833110
Kelvin Brito <britok30@gmail.com>
2020-08-27 00:27:56 -0400

    add socket.io connection

--- server/index.js
+++ server/index.js
@@ -4,9 +4,20 @@ const http = require('http');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const router = require('./router');
 
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected!!!');
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected!!');
+    });
+});
+
+app.use(router);
 
 server.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
