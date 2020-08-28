6da262cd97b830891f85fb3e2d79bef098452c0c
Kelvin Brito <britok30@gmail.com>
2020-08-28 00:15:52 -0400

    socket io setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,10 +11,32 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('a user connected!!!');
-
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if (error) return callback(error);
+
+        socket.emit('message', {
+            user: 'admin',
+            text: `${user.name}, welcome to the room ${user.room}`,
+        });
+
+        socket.broadcast.to(user.room).emit('message', {
+            user: 'admin',
+            text: `${user.name}, has joined`,
+        });
+
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
     });
 
     socket.on('disconnect', () => {
